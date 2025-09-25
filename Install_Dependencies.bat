8f669cbf1ef9f728d6d66a9e3a30d036cfb47f59
bladestech <87785719+bladestech@users.noreply.github.com>
2025-09-26 01:32:05 +0300

    Update Install_Dependencies.bat

--- Install_Dependencies.bat
+++ Install_Dependencies.bat
@@ -2,5 +2,6 @@
 pip install PyQt6 PyQt6-WebEngine pyinstaller
 echo.
 echo.
-echo Dependencies should now be installed, unless any errors occured. Enjoy using Pymake!
+echo Dependencies should now be installed, unless any errors occured. Enjoy using WebSoftPy!
+
 pause
